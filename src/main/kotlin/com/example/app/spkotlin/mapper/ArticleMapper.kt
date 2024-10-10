71401069adb8a8ab44fc70e06732c0a92f1ef9ee
Gisele <gisesonia@gmail.com>
2024-10-10 14:22:28 -0300

    feat: mapper dto

--- src/main/kotlin/com/example/app/spkotlin/mapper/ArticleMapper.kt
+++ src/main/kotlin/com/example/app/spkotlin/mapper/ArticleMapper.kt
@@ -1,6 +1,7 @@
 package com.example.app.spkotlin.mapper
 
 import com.example.app.spkotlin.dto.ArticleDTO
+import com.example.app.spkotlin.extensions.toSlug
 import com.example.app.spkotlin.model.Article
 
 // Função de extensão para converter Article em ArticleDTO
@@ -16,8 +17,8 @@ fun Article.toDTO(): ArticleDTO {
 // Função de extensão para converter ArticleDTO em Article
 fun ArticleDTO.toEntity(): Article {
     return Article(
-        title = this.title,
+        title = this.title ?: "",
         content = this.content,
-        slug = this.title?.toSlug()  // Certifique-se de ter a função toSlug implementada
+        slug = this.title?.toSlug() ?: ""
     )
 }
