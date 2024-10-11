142b13159337da0681ebfa834e4c8cef2436a626
Gisele <gisesonia@gmail.com>
2024-10-10 23:30:52 -0300

    chore: habilitando pasta static

---
+++ src/main/kotlin/com/example/app/spkotlin/config/Webconfig.kt
@@ -0,0 +1,14 @@
+package com.example.app.spkotlin.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/static/**")
+            .addResourceLocations("classpath:/static/")
+    }
+}
