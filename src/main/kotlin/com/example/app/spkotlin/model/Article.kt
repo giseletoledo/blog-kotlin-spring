28e138a2ddb06697cd466439658327a48c83b807
Gisele <gisesonia@gmail.com>
2024-10-12 23:55:50 -0300

    refactor: limite de caracteres nos campos

--- src/main/kotlin/com/example/app/spkotlin/model/Article.kt
+++ src/main/kotlin/com/example/app/spkotlin/model/Article.kt
@@ -1,18 +1,19 @@
 package com.example.app.spkotlin.model
 
 import com.example.app.spkotlin.extensions.toSlug
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import java.time.LocalDateTime
 
 @Entity
 class Article(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
+    @Column(length = 150)
     var title: String,
+    @Column(length = 5000)
     var content: String,
     val createdAt: LocalDateTime = LocalDateTime.now(),
     var slug: String = title.toSlug()
-)
+) {
+    var formattedCreatedAt: String? = null
+}
